a79828ebca216f3674ec4940702081756c5046b8
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-10-28 13:07:24 -0300

    feat: adding phoneNumber and hiringCode dtos

--- library/src/main/java/com/api/library/dtos/AdministratorRecordDTO.java
+++ library/src/main/java/com/api/library/dtos/AdministratorRecordDTO.java
@@ -1,6 +1,8 @@
 package com.api.library.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record AdministratorRecordDTO(@NotBlank String name,@NotBlank String email,@NotBlank String cpf) {
+public record AdministratorRecordDTO(@NotBlank String name,@NotBlank String email,@NotBlank String documentCPF,
+                                     @NotBlank @NotNull String phoneNumber, @NotBlank @NotNull String hiringCode) {
 }
