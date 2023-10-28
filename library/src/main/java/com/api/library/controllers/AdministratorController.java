01f501b6bf00ab082dae14f624055e2f77bb17af
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-10-28 14:17:29 -0300

    feat: creating method to save new administrators using service class

--- library/src/main/java/com/api/library/controllers/AdministratorController.java
+++ library/src/main/java/com/api/library/controllers/AdministratorController.java
@@ -3,23 +3,28 @@ package com.api.library.controllers;
 import com.api.library.dtos.AdministratorRecordDTO;
 import com.api.library.entities.Administrator;
 import com.api.library.repositories.AdministratorRepository;
+import com.api.library.services.AdministratorService;
+import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/administrators")
 public class AdministratorController {
 
     @Autowired
-    AdministratorRepository administratorRepository;
+    AdministratorService administratorService;
 
-    @PostMapping("/new-adm")
-    public ResponseEntity<Administrator> saveNewAdm(AdministratorRecordDTO administratorRecordDTO){
+    @PostMapping
+    public ResponseEntity<Administrator> saveNewAdm(@RequestBody @Valid AdministratorRecordDTO administratorRecordDTO){
         var adm = new Administrator();
         BeanUtils.copyProperties(administratorRecordDTO, adm);
-        return ResponseEntity.status(HttpStatus.CREATED).body(administratorRepository.save(adm));
+        return ResponseEntity.status(HttpStatus.CREATED).body(administratorService.saveNewAdm(adm));
     }
 }
