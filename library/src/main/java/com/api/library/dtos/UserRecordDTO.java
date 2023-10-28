79fd1b2d71b3f5e5ef1bc9e2704951c64ec0ea40
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-10-28 12:59:09 -0300

    feat: adding phone number dto

--- library/src/main/java/com/api/library/dtos/UserRecordDTO.java
+++ library/src/main/java/com/api/library/dtos/UserRecordDTO.java
@@ -3,5 +3,6 @@ package com.api.library.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UserRecordDTO(@NotBlank String name,@NotBlank String email,@NotBlank @NotNull String cpf) {
+public record UserRecordDTO(@NotBlank String name,@NotBlank String email,@NotBlank @NotNull String documentCPF,
+                            @NotBlank @NotNull String phoneNumber) {
 }
