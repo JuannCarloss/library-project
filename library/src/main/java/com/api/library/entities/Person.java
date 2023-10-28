70188b863d2c54eb2e63a6200e10bbe54f960a32
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-10-28 11:27:21 -0300

    feat: mapped Person class

---
+++ library/src/main/java/com/api/library/entities/Person.java
@@ -0,0 +1,22 @@
+package com.api.library.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class Person extends EntityID{
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "telefone", unique = true, nullable = false)
+    private String phoneNumber;
+
+}
