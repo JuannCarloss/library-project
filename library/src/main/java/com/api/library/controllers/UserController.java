b6b0572fa409a1a439beddc7e974c154cf39cc31
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-10-30 20:03:02 -0300

    feat: using UserService

--- library/src/main/java/com/api/library/controllers/UserController.java
+++ library/src/main/java/com/api/library/controllers/UserController.java
@@ -3,34 +3,30 @@ package com.api.library.controllers;
 import com.api.library.dtos.UserRecordDTO;
 import com.api.library.entities.User;
 import com.api.library.repositories.UserRepository;
+import com.api.library.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
-    UserRepository userRepository;
+    private UserService userService;
 
 
-    @PostMapping("/new-users")
+    @PostMapping
     public ResponseEntity<User> saveUser(@RequestBody @Valid UserRecordDTO userRecordDTO){
         var user = new User();
         BeanUtils.copyProperties(userRecordDTO, user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(user));
+        User save = userService.saveNewUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(save);
     }
 
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> listAllUsers(){
-        return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
-    }
 }
