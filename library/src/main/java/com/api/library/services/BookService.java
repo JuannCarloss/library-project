ead0f07f7963cd53cb75449d819ef685ce2ab263
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-11-10 19:19:22 -0300

    feat: adding method to change availability

--- library/src/main/java/com/api/library/services/BookService.java
+++ library/src/main/java/com/api/library/services/BookService.java
@@ -1,9 +1,12 @@
 package com.api.library.services;
 
 import com.api.library.entities.Book;
+import com.api.library.enums.Availability;
 import com.api.library.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -13,5 +16,19 @@ public class BookService {
 
     public Book saveNewBook(Book book){
         return bookRepository.save(book);
+    }
+
+    public void updateStatus(Long id) throws Exception{
+        Optional<Book> book0 = bookRepository.findById(id);
+        if (book0.isPresent()) {
+            var book = book0.get();
+            if (book.getAvailability() == Availability.AVAILABLE){
+                book.setAvailability(Availability.UNAVAILABLE);
+                bookRepository.save(book);
+            }
+            }else {
+            throw new Exception("livro não existe no banco de dados");
+        }
+
     }
 }
--- library/src/main/java/com/api/library/services/UserService.java
+++ library/src/main/java/com/api/library/services/UserService.java
@@ -1,9 +1,12 @@
 package com.api.library.services;
 
 import com.api.library.entities.User;
+import com.api.library.enums.Availability;
 import com.api.library.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,5 +16,18 @@ public class UserService {
 
     public User saveNewUser(User user){
         return userRepository.save(user);
+    }
+
+    public void updateStatus(Long id) throws Exception{
+        Optional<User> user0 = userRepository.findById(id);
+        if (user0.isPresent()) {
+            var user = user0.get();
+            if (user.getAvailability() == Availability.AVAILABLE){
+                user.setAvailability(Availability.UNAVAILABLE);
+                userRepository.save(user);
+            }
+        }else {
+            throw new Exception("usuário não existe na base de dados");
+        }
     }
 }
