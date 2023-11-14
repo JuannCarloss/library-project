182b612e590c3594f65a55b5c50dc3a992145314
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-11-13 21:19:37 -0300

    feat: user devolution method

--- library/src/main/java/com/api/library/services/UserService.java
+++ library/src/main/java/com/api/library/services/UserService.java
@@ -1,11 +1,15 @@
 package com.api.library.services;
 
+import com.api.library.entities.LoanBooks;
 import com.api.library.entities.User;
 import com.api.library.enums.Availability;
+import com.api.library.repositories.BookRepository;
+import com.api.library.repositories.LoanBooksRepository;
 import com.api.library.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
@@ -13,6 +17,11 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private LoanBooksRepository loanBooksRepository;
+
+
 
     public User saveNewUser(User user){
         return userRepository.save(user);
@@ -25,9 +34,28 @@ public class UserService {
             if (user.getAvailability() == Availability.AVAILABLE){
                 user.setAvailability(Availability.UNAVAILABLE);
                 userRepository.save(user);
-            }
-        }else {
-            throw new Exception("usuário não existe na base de dados");
+            }else {
+                throw new Exception("Usuário não está disponível para realizar empréstimo!");
+                }
+            } else {
+            throw new Exception("usuário não existe na base de dados!");
         }
+    }
+
+    public LoanBooks returnBook(Long loanID)throws Exception{
+        Optional<LoanBooks> optional = loanBooksRepository.findById(loanID);
+            if (optional.isPresent()){
+                var loan = optional.get();
+                if (loan.getUser().getAvailability() == Availability.UNAVAILABLE && loan.getBook().getAvailability() == Availability.UNAVAILABLE) {
+                    loan.getUser().setAvailability(Availability.AVAILABLE);
+                    loan.getBook().setAvailability(Availability.AVAILABLE);
+                    loan.setDevolutionDate(LocalDateTime.now());
+                    return loanBooksRepository.save(loan);
+                }else{
+                    throw new Exception("Usuário não tem nenhum livro emprestado!");
+                }
+            }else {
+                throw new Exception("Empréstimo não cadastrado na base de dados!");
+            }
     }
 }
