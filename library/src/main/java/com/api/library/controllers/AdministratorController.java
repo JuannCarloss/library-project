c353a18f0caec3154f2f80d172449aed5ff7b48e
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-11-15 12:30:37 -0300

    feat: adding post mapping to save new loan

--- library/src/main/java/com/api/library/controllers/AdministratorController.java
+++ library/src/main/java/com/api/library/controllers/AdministratorController.java
@@ -2,8 +2,10 @@ package com.api.library.controllers;
 
 import com.api.library.dtos.AdministratorRecordDTO;
 import com.api.library.entities.Administrator;
+import com.api.library.entities.LoanBooks;
 import com.api.library.repositories.AdministratorRepository;
 import com.api.library.services.AdministratorService;
+import com.api.library.services.LoanBooksService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdministratorController {
 
     @Autowired
-    AdministratorService administratorService;
+    private AdministratorService administratorService;
+
+    @Autowired
+    private LoanBooksService loanBooksService;
 
     @PostMapping
     public ResponseEntity<Administrator> saveNewAdm(@RequestBody @Valid AdministratorRecordDTO administratorRecordDTO){
         var adm = new Administrator();
         BeanUtils.copyProperties(administratorRecordDTO, adm);
         Administrator save = administratorService.saveNewAdm(adm);
+        return ResponseEntity.status(HttpStatus.CREATED).body(save);
+    }
+    @PostMapping("/loans")
+    public ResponseEntity saveNewLoan(@RequestBody @Valid LoanBooks loanBooks) throws Exception {
+        LoanBooks save = loanBooksService.saveNewLoan(loanBooks);
         return ResponseEntity.status(HttpStatus.CREATED).body(save);
     }
 }
