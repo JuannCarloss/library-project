1665842857409c924707be2901c95d5db0ae6188
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-11-15 12:31:10 -0300

    feat: using book service

--- library/src/main/java/com/api/library/controllers/BookController.java
+++ library/src/main/java/com/api/library/controllers/BookController.java
@@ -3,34 +3,34 @@ package com.api.library.controllers;
 import com.api.library.dtos.BookRecordDTO;
 import com.api.library.entities.Book;
 import com.api.library.repositories.BookRepository;
+import com.api.library.services.BookService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
 
     @Autowired
-    BookRepository bookRepository;
+    BookService bookService;
 
 
-    @PostMapping("/new-books")
+    @PostMapping
     public ResponseEntity<Book> saveNewBook(@RequestBody @Valid BookRecordDTO bookRecordDTO){
         var book = new Book();
         BeanUtils.copyProperties(bookRecordDTO, book);
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookRepository.save(book));
+        Book save = bookService.saveNewBook(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(save);
     }
 
-    @GetMapping("/books")
-    public ResponseEntity<List<Book>> getAllBooks(){
-        return ResponseEntity.status(HttpStatus.OK).body(bookRepository.findAll());
-    }
+//    @GetMapping
+//    public ResponseEntity<List<Book>> getAllBooks(){
+//        return ResponseEntity.status(HttpStatus.OK).body();
+//    }
 }
