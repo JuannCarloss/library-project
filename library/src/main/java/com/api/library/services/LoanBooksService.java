1879d372bb24f1ba4b50430272f1fb91d88a1bf5
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-11-16 17:30:21 -0300

    feat: adding get and delete methods

--- library/src/main/java/com/api/library/services/LoanBooksService.java
+++ library/src/main/java/com/api/library/services/LoanBooksService.java
@@ -5,13 +5,15 @@ import com.api.library.entities.LoanBooks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LoanBooksService {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
     @Autowired
-    BookService bookService;
+    private BookService bookService;
     @Autowired
     private LoanBooksRepository repository;
 
@@ -23,5 +25,17 @@ public class LoanBooksService {
         userService.updateStatus(loan.getUser().getId());
         bookService.updateStatus(loan.getBook().getId());
         return repository.save(loan);
+    }
+
+    public List<LoanBooks> findAllLoans(){
+        return repository.findAll();
+    }
+
+    public LoanBooks findLoanById(Long id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deleteLoan(Long id){
+        repository.deleteById(id);
     }
 }
