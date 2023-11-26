fa4603cb4a0f5935d593127011a3507e3b0859ba
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2023-11-26 12:10:17 -0300

    feat: implementing abstract controller

---
+++ library/src/main/java/com/api/library/enterprise/ValidationException.java
@@ -0,0 +1,7 @@
+package com.api.library.enterprise;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
