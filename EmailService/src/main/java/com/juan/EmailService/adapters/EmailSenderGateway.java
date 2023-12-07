fbb6a7ef63fc13d1a3c04789f4e19849d2bd47cd
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-12-07 19:46:11 -0300

    deleting other packages

--- EmailService/src/main/java/com/juan/EmailService/adapters/EmailSenderGateway.java
+++
@@ -1,5 +0,0 @@
-package com.juan.EmailService.adapters;
-
-public interface EmailSenderGateway {
-    void sendEmail(String to, String subject, String body);
-}
--- EmailService/src/main/java/com/juan/EmailService/core/EmailSenderUseCase.java
+++
@@ -1,5 +0,0 @@
-package com.juan.EmailService.core;
-
-public interface EmailSenderUseCase {
-    void sendEmail(String to, String subject, String body);
-}
